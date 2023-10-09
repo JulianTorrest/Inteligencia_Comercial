8eacc06ba5a1059a9e1f1e1407934df113601ce3
JulianTorrest <43328220+JulianTorrest@users.noreply.github.com>
2023-10-08 22:00:49 -0500

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def load_data():
     st.title("Inteligencia Comercial, sector Abonos. Exportación del año 2023.")
 
     # Introduce tu token válido aquí o considera una forma segura de almacenar y recuperar el token.
-    token = "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"
+    token = "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"
 
     file_options = {
         "CO-EX-ABONO-SALIDA.xlsx": "JulianTorrest/Inteligencia_Comercial/contents/Abonos/CO-EX-ABONO-SALIDA.xlsx",
